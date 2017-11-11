f59d74bf9dfcff07018cc4b5693d51bdf9bbf87f
David Muckle <dvdmuckle@dvdmuckle.xyz>
2017-11-11 00:16:58 -0500

    Add jokesdb, add HTTP server implementation

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM golang:latest
 ENV HOME=/root
 RUN go get github.com/dvdmuckle/curl-a-joke
+COPY jokes.db /root/
 ENTRYPOINT ["/go/bin/curl-a-joke"]
--- curl-a-joke.go
+++ curl-a-joke.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	_ "github.com/jinzhu/gorm"
+	"net/http"
+)
 
+type Joke struct {
+	ID   uint `gorm:"primary_key"`
+	Joke string
+}
+
+var dbFile string
+var jokePort string
+
+func requestjoke(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "There should be a joke here. Not yet, but soon.")
+}
 func main() {
-	fmt.Println("vim-go")
+	dbPtr := flag.String("jokedb", "jokes.db", "Location to the jokes database")
+	portPtr := flag.Int("port", 8080, "Port for server")
+	flag.Parse()
+	dbFile = *dbPtr
+	jokePort = fmt.Sprintf(":%d", *portPtr)
+	http.HandleFunc("/", requestjoke)
+	http.ListenAndServe(jokePort, nil)
 }
